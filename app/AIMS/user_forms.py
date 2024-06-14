865a086749eb4f1a28224a35b87e193614b03fe1
a1105510 <a1105510@mail.nuk.edu.tw>
2024-06-15 05:35:06 +0800

    fix: tel可為空

--- app/AIMS/user_forms.py
+++ app/AIMS/user_forms.py
@@ -10,8 +10,8 @@ class UserForm(FlaskForm):
     passwd = PasswordField('密碼', validators=[DataRequired()])
     name = StringField('名稱', validators=[DataRequired()])
     email = StringField('電子郵件', validators=[DataRequired(), Email('請輸入正確的電子郵件地址')])
-    tel = StringField('電話', validators=[DataRequired()])
-    type = HiddenField('使用者身分')
+    tel = StringField('電話')
+    type = HiddenField('使用者身分', validators=[DataRequired()])
     submit = SubmitField('確認')
 
 
@@ -29,8 +29,7 @@ class AdvisorForm(UserForm):
 class StudentForm(UserForm):
     dept = StringField('系所', validators=[DataRequired()])
     enroll_year = IntegerField('入學年分', validators=[DataRequired()])
-    sex = SelectField('性別', choices=[('1', '男'), ('0', '女')], validators=[DataRequired()])
-    sex = SelectField('性別', choices=[('1', '男'), ('0', '女')], validators=[DataRequired()])
+    sex = SelectField('性別', choices=[('1', '男'), ('0', '女')])
     home_addr = StringField('家中地址')
     home_tel = StringField('家裡電話')
     contact_name = StringField('聯絡人姓名')
