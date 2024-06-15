ad1370b70fa2f6f84cc0e4f56a6f7dfc751ab468
Nat Su <95582835+natsu5510@users.noreply.github.com>
2024-06-16 01:08:49 +0800

    feat: 使用者更改個人資料

--- app/AIMS/update_personal_information.py
+++ app/AIMS/update_personal_information.py
@@ -1,42 +1,63 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import login_required, current_user
+from flask_wtf import FlaskForm
 from wtforms.fields.simple import SubmitField, PasswordField
 from wtforms.validators import DataRequired, EqualTo
-from flask_wtf import FlaskForm
 
+from app.AIMS.user_forms import StudentForm, AdvisorForm, LandlordForm
 from app.extensions import db, bcrypt
-from app.models import User
-from app.AIMS.user_forms import StudentForm
 
 update_personal_information = Blueprint('update_personal_information', __name__)
 
-class PersonalUpdateForm(StudentForm):
-    submit = SubmitField('更新')
 
-class PasswordResetForm(FlaskForm):
+class StudentUpdateForm(StudentForm):
+    submit = SubmitField('確認更改')
+
+
+class AdvisorUpdateForm(AdvisorForm):
+    submit = SubmitField('確認更改')
+
+
+class LandlordUpdateForm(LandlordForm):
+    submit = SubmitField('確認更改')
+
+
+class PasswordUpdateForm(FlaskForm):
     old_password = PasswordField('請輸入原本的密碼', validators=[DataRequired()])
     new_password = PasswordField('重新輸入新密碼', validators=[DataRequired()])
-    confirm_password = PasswordField('確認新密碼', validators=[DataRequired(), EqualTo('new_password', message='密碼不一致')])
+    confirm_password = PasswordField('確認新密碼',
+                                     validators=[DataRequired(), EqualTo('new_password', message='密碼不一致')])
     submit = SubmitField('變更密碼')
+
 
 @update_personal_information.route('/update_personal_information', methods=['GET', 'POST'])
 @login_required
 def update_form():
     user = current_user
-    form = PersonalUpdateForm(obj=user)
+
+    if user.type == 'student':
+        form = StudentUpdateForm(obj=user)
+    elif user.type == 'advisor':
+        form = AdvisorUpdateForm(obj=user)
+    elif user.type == 'landlord':
+        form = LandlordUpdateForm(obj=user)
+    else:
+        flash('使用者身分錯誤', 'danger')
+        return redirect(url_for('AIMS/update_personal_information.html', updateForm=None))
 
     if form.validate_on_submit():
         form.populate_obj(user)
         db.session.commit()
         flash('個人資料已更新', 'success')
         return redirect(url_for('update_personal_information.update_form'))
-    
+
     return render_template('AIMS/update_personal_information.html', updateForm=form)
 
-@update_personal_information.route('/reset_password', methods=['GET', 'POST'])
+
+@update_personal_information.route('/update_password', methods=['GET', 'POST'])
 @login_required
-def reset_password():
-    form = PasswordResetForm()
+def update_password():
+    form = PasswordUpdateForm()
     if form.validate_on_submit():
         if not bcrypt.check_password_hash(current_user.passwd, form.old_password.data):
             flash('原本的密碼不正確', 'danger')
@@ -49,4 +70,4 @@ def reset_password():
             db.session.commit()
             flash('密碼已成功更改', 'success')
             return redirect(url_for('update_personal_information.update_form'))
-    return render_template('AIMS/reset_password.html', form=form)
+    return render_template('AIMS/update_password.html', form=form)
